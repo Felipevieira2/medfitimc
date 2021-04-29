c61f97fb130a0ecf3d0ab31a6b2086ff687a958f
felipe <felipy_erivaldo@hotmail.com>
2021-04-29 18:39:20 -0300

    adicionado form de resultado

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,62 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import React, { useCallback, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useLocalStorage } from './utils/localStorage'
 
 export default function Home() {
+  const [nome, setNome ] = React.useState(''); 
+  const [phone, setPhone ] = React.useState('');
+  const [altura, setAltura ] = React.useState('');
+  const [peso, setPeso ] = React.useState('');
+  const router = useRouter();
+
+  useEffect(() => {
+   
+    if ( window.localStorage.getItem('lead') ){
+      window.localStorage.removeItem('lead') 
+    }   
+
+  }, [])
+
+
+  const onPressSubmit = (e) => {
+    e.preventDefault();
+    
+    var imc = Number((peso.replace(',', '.') / (altura.replace(',', '.') * altura.replace(',', '.'))) ).toFixed(2);
+    let textResult1;
+    let textResult2;
+
+    if( imc < 18.5 ) {
+      textResult1 = "Infelizmente você está abaixo de seu peso.";
+      textResult2 = "O seu peso deveria ser no mínimo 67 kg. Assim, para ganhar peso de forma saudável e sem ganhar barriga, você deve continuar praticando atividade física, aumentar o volume das refeições e comer a cada 3 horas. "
+    }
+    if( imc >= 18.5 && imc < 24.9 ) {
+      textResult1 = "Parabens! Você está no seu peso ideal.";
+      textResult2 = "De acordo com as informações passadas, o seu peso pode variar entre 67 kg e 90 kg. "
+    }else if( imc >= 24.9  && imc <= 30 ) {
+      textResult1 = "Infelizmente você está acima do peso.";
+      textResult2 = "O seu peso ideal pode variar entre 67 kg e 90 kg por isso para emagrecer com saúde é importante comer mais frutas e verduras além de praticar exercício físico entre 2 e 3 vezes por semana, ingerindo 3035 calorias por dia"
+    }else if( imc >= 30.0 && imc <= 34.9 ) {
+      textResult1 = "Infelizmente você está com Obesidade";  
+      textResult2 = "Você deve ainda falar com seu médico para fazer exames de sangue para saber se o seu colesterol e triglicerídeos estão bem. Um nutricionista também pode te ajudar a emagrecer comendo bem, mas além da dieta é também importante fazer exercícios físicos bem orientado por um preparador físico para emagrecer mais rápido."    
+    }
+
+    let newLead = { 
+      nome,
+      phone,
+      altura,
+      peso,
+      imc,
+      textResult1,
+      textResult2
+    };   
+
+    window.localStorage.setItem('lead', JSON.stringify(newLead));
+    
+    router.push('/resultado');
+  }
+
   return (
     <div className="p-4 bg-gradient-to-r from-blue-100">
       <Head>
@@ -65,8 +120,8 @@ export default function Home() {
           <div className="mt-8 w-full md:w-9/12">
             <a href="#form" target="_self">
               <button className=" bg-green-300 text-xl font-bold 
-                      px-9 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mb-1 
-                      transition-all duration-150"  type="button">Calcular IMC</button>
+                px-9 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mb-1 
+                transition-all duration-150"  type="button">Calcular IMC</button>
             </a>
           </div>
           <br></br>
@@ -84,34 +139,58 @@ export default function Home() {
           <div id="form" className="mt-24 bg-white sm:w-12/12 lg:w-4/12 md:7/12 shadow-md w-full  ml-auto mr-auto">
             <div className="py-8 px-8 rounded-xl">
               <h1 className="font-medium text-2xl mt-3 text-center">Calculadora IMC</h1>
-              <form action="" className="mt-6">
+              <form id="formIMC" action="/resultado" method="POST" className="mt-6">
                 <div className="my-5 text-sm">
                   <label htmlFor="nome" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Nome *</label>
-                  <input type="text" autoFocus="" id="nome"
+                  <input type="text" autoFocus="" id="nome" 
+                    onChange={(e) => { 
+                      setNome(e.target.value);                                           
+                     }}
                     required className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
                     placeholder="Nome" />
                 </div>
                 <div className="my-5 text-sm">
                   <label htmlFor="phone" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Telefone *</label>
-                  <input type="tel" id="phone" name="phone"
+                  <input type="tel" id="phone" name="phone" maxLength="12"
+                    onChange={(e) => {
+                     
+                      e.target.value = e.target.value.replace(/\D/g, ""); 
+                      e.target.value = e.target.value.replace(/^(\d{2})(\d)/, "$1-$2")
+                      setPhone(e.target.value); 
+                    }}
                     required className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                    placeholder="Telefone" />
+                    placeholder="Telefone"/>
 
                 </div>
                 <div className="my-5 text-sm">
                   <label htmlFor="altura" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Altura (ex.: 1,70) *</label>
-                  <input type="text" autoFocus="" id="altura"
+                  <input type="text" autoFocus="" id="altura" maxLength="4"
+                    onChange={(e) => { 
+                  
+                      e.target.value = e.target.value.replace(/\D/g, "");
+                      e.target.value = e.target.value.replace(/(\d)(\d{2})$/, "$1,$2");
+                      e.target.value = e.target.value.replace(/(?=(\d{3})+(\D))\B/g, ".");
+                      setAltura(e.target.value);
+                    }}
                     required className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
                     placeholder="Altura" />
                 </div>
                 <div className="my-5 text-sm">
-                  <label htmlFor="phone" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Peso (ex.: 69,2) *</label>
-                  <input type="text" id="peso"
+                  <label htmlFor="phone"
+                   className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Peso (ex.: 69,2) *</label>
+                  <input type="text" id="peso" maxLength="6"
+                    onChange={(e) => {
+                   
+                 
+                    e.target.value = e.target.value.replace(/\D/g, "");
+                    e.target.value = e.target.value.replace(/(\d)(\d{2})$/, "$1,$2");
+                    e.target.value = e.target.value.replace(/(?=(\d{3})+(\D))\B/g, "$1,$2");
+                    setPeso(e.target.value);
+                  }}
                     required className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
                     placeholder="peso" />
                 </div>
-
-                <button className="block text-center text-white bg-gray-800 p-3 duration-300 rounded-sm hover:bg-black w-full">Calcular</button>
+                <button onClick={ (e) => {  onPressSubmit(e) } } type="button" className="block text-center text-white bg-gray-800 p-3 duration-300 rounded-sm hover:bg-black w-full">Calcular</button>
               </form>
 
 
