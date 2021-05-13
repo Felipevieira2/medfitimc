60dd225f2fd3c26e70a1cff6f9b567e375d2d012
unknown <felipy_erivaldo@hotmail.com>
2021-05-12 23:33:21 -0300

    test pixel

--- pages/resultado.js
+++ pages/resultado.js
@@ -4,6 +4,8 @@ import { faWhatsapp } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as fbq from '../lib/fbPixel'
 import FacebookPixel from '../components/FacebookPixel'
+import Head from 'next/head'
+import { FB_PIXEL_ID } from '../lib/fbPixel'
 
 export default function Resultado() {
   const [lead, setLead] = React.useState('');
@@ -27,6 +29,32 @@ export default function Resultado() {
   }
   return (
     <FacebookPixel>
+      <Head>
+        {/* Global Site Code Pixel - Facebook Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', ${FB_PIXEL_ID});
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
+      </Head>
       <div className="bg-gradient-to-r from-blue-100">
         <div className="container flex flex-wrap px-4 md:px-0 max-w-6xl mx-auto">
           <div className="inline-flex items-center justify-center w-full shadow-sm  my-6 rounded-sm">
