b174a2385f13c971c0a654ec7e3d580886c9f15b
felipe <felipy_erivaldo@hotmail.com>
2021-05-13 19:16:27 -0300

    adicionado meta tag do facebook

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-         
+          <meta name="facebook-domain-verification" content="dmul7k34k1v4nsi55jas5knv1t04hd" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
     
